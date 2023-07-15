34606c623057ce8abd03522458ba802573030b13
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-07-15 19:44:12 +0700

    first commit

---
+++ clock.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 10:02:26 2021
+
+@author: wearnes
+"""
+from tkinter import *
+from time import *
+
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+    
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+    
+    date_string = strftime("%d %B %Y")
+    date_label.config(text=date_string)
+    
+    time_label.after(1000, update)
+
+window = Tk()
+
+time_label = Label(window,font=("Arial", 50),fg="white",bg="black")
+time_label.pack()
+
+day_label = Label(window,font=("Arial", 20))
+day_label.pack()
+
+date_label = Label(window,font=("Arial", 30))
+date_label.pack()
+
+update()
+
+window.mainloop()
